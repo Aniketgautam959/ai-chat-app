49a1d9639fca721a6759940540e92ae52e0148bf
Aniketgautam959 <164925581+Aniketgautam959@users.noreply.github.com>
2025-08-30 14:20:10 +0530

    ok

---
+++ ai-chat-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='grid grid-cols-5'>
+    <div className='col-span-1'>
+
+    </div>
+    <div className='col-span-4'>
+
+    </div>
+   </div>
+  )
+}
+
+export default App
